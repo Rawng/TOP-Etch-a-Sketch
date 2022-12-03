a3600a1919eaf229773db5cae4ee839862ad47b5
Rawng <Rawng@users.noreply.github.com>
2022-12-03 11:04:32 -0500

    Move painting logic into separate function

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
+function paint(event) {
+    let color = colorMode();
+    if (event.altKey) {
+        event.target.classList.add('hovered');
+    }
+}
+
+function colorMode() {
+    let colorModeRadio = document.getElementsByName('color');
+    colorModeRadio.forEach((button) => {
+        if (button.checked) {
+            return button.value
+        }
+    })
+}
+
 const gridCells = document.querySelectorAll('.gridCell');
 gridCells.forEach((cell) => {
     cell.addEventListener('mouseover', function (e) {
         console.log(e);
-        if (e.altKey) {
-            cell.classList.add('hovered');
-        }
+        paint(e);
     });
 });
